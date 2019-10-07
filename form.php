af4dbc6bafd4a328eb5112f742309cd899809252
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-10-07 14:57:16 +0200

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -1,21 +1,3 @@
-<?php
-
-if(isset($_GET["email"]) && !empty($_GET["email"])) {
-    //Valideer email
-    if(filter_var($_GET["email"], FILTER_VALIDATE_EMAIL)){
-        echo "E-mailadres is geldig!";
-    }
-    else {
-        "E-mailadres is ongeldig! Voer een geldige email in";
-    }
-}
-else {
-    echo "Voer een email in";
-}
-
-
-?>
-
 <!DOCTYPE html>
 
 <html>
@@ -25,13 +7,28 @@ else {
 </head>
 
 <body>
+    <?php
+
+    if (isset($_GET["email"]) && !empty($_GET["email"])) {
+        //Valideer email
+        if (filter_var($_GET["email"], FILTER_VALIDATE_EMAIL)) {
+            echo "E-mailadres is geldig!";
+        } else {
+            "E-mailadres is ongeldig! Voer een geldige email in";
+        }
+    } else {
+        echo "Voer een email in";
+    }
+
+    ?>
+
     <div>
         <form name="email" action="" method="get">
-        <div>
-            <label for="email">Voer hier je email in</label>
-            <input type="email" name="email" id="email" required/>
-            <input type="submit" value="Verstuur"/>
-        </div>
+            <div>
+                <label for="email">Voer hier je email in</label>
+                <input type="email" name="email" id="email" required />
+                <input type="submit" value="Verstuur" />
+            </div>
         </form>
     </div>
 
